e1987f35501a2c34893922394f295166c3f6ba25
hemfreitas11 <hemfreitas11@users.noreply.github.com>
2021-05-02 21:47:09 -0300

    'teste'

--- index.js
+++ index.js
@@ -394,6 +394,10 @@ client.once('ready', async () => {
 					res.send('Awake!')
 					console.log(req.body)
 				})
+				app.get('/m12j-31as-k4dk-asjd-klj1-2349-8d5a', (req, res) => {
+					res.send('Awake!')
+					console.log(req.body)
+				})
 				app.listen(process.env.PORT, '0.0.0.0', () => {
 					console.log('Backend Ligado')
 				})
