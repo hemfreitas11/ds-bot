aacf42deb6b635a6aa23acb89521e3534c642af4
hemfreitas11 <hemfreitas11@users.noreply.github.com>
2021-05-10 20:16:49 -0300

    fix

--- index.js
+++ index.js
@@ -1246,12 +1246,16 @@ function startBkDuelPaymentListeners() {
 }
 
 function startPortListener() {
+	app.post('/m12j-31as-k4dk-asjd-klj1-2349-8d5a', (req, res) => {
+		res.send('a')
+	})
 	app.get('/wakeup', (req, res) => {
 		res.send('Awake!')
 	})
 	app.listen(process.env.PORT, '0.0.0.0', () => {
 		console.log('Backend Ligado')
 	})
+	
 }
 
 function startStatusAnimation() {
